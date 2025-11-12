e8a7ba2337140bfd1e9fff542303e4a4ec9f0222
andrewpr52 <andrewpr52@gmail.com>
2025-11-11 22:35:03 -0600

    fixed mysql connection env var references

--- src/utils/DBUtil.js
+++ src/utils/DBUtil.js
@@ -8,11 +8,10 @@ class DBUtil {
 
         try {
             connection = await mysql.createConnection({
-                host: process.env.MYSQLHOST,
-                user: process.env.MYSQLUSER,
-                password: process.env.MYSQLPASSWORD,
-                database: process.env.MYSQLDATABASE,
-                port: process.env.MYSQLPORT
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                database: process.env.DB_NAME
             });
 
             const [rows] = await connection.execute(sql, params);
